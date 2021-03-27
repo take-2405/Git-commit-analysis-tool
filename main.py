74af236865a474d2d73281a20ac348a978c5badc
take-2405 <take12.wa@icloud.com>
2021-03-28 01:18:45 +0900

    サーバの実行ポート変更

--- main.py
+++ main.py
@@ -18,5 +18,6 @@ def get_log():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    # app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False)
 
