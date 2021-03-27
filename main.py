ab5e823e0a7f4f337c563522f7e6ae26ae605697
take-2405 <take12.wa@icloud.com>
2021-03-28 00:19:59 +0900

    デプロイのための最終調整

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from pkg import analysis_git
 from flask import Flask, jsonify, request
 
-app = Flask("git-analysis")
+app = Flask('__name__')
 
+app.config['JSON_AS_ASCII'] = False
 
 @app.route("/get/log", methods=["GET"])
 def get_log():
     git_name = request.args.get("gitName")
+    if git_name == "":
+        return jsonify({'message': 'header owner name is null'}), 400
     repository_name = request.args.get("repositoryName")
-    analysis_git.analysis_git_log(git_name, repository_name)
-    return "aaaa"
+    if repository_name == "":
+        return jsonify({'message': 'header repository name is null'}), 400
+    git_log = analysis_git.analysis_git_log(git_name, repository_name)
+    return jsonify({"git_log": git_log}), 200
 
-app.run(port=8080)
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
 
--- pkg/analysis_git.py
+++ pkg/analysis_git.py
@@ -3,57 +3,60 @@ import datetime
 
 
 def analysis_git_log(user_name, repository_name):
+    git_log = []
     # 日付が2つ重複しているので1つを表示しないためのフラグ
     date_flag = True
     # コミットメッセージの終わりを識別するためのフラグ
     message_flag = False
     # 日付比較のフラグ
     compare_flag = False
+    # マージ判定フラグ
+    merge_flag = False
     # 目的のリポジトリのログを取得
     site = requests.get("https://api.github.com/repos/" + user_name + "/" + repository_name + "/commits")
-    site_info = site.text
     # 目的のログ部分を切り取る
-    for i in range(0, len(site_info)):
+    for i in range(0, len(site.text)):
+        # メッセージを選別
+        if site.text[i] == "m" and site.text[i + 1] == "e" and site.text[i + 2] == "s" \
+                and site.text[i + 3] == "s" and site.text[i + 4] == "a" and site.text[i + 5] == "g" and site.text[i + 6] == "e":
+            message_start = i + 10
+            for message_end in range(message_start, len(site.text)):
+                if site.text[message_end] == "\"":
+                    if message_flag:
+                        git_log.append(site.text[message_start:message_end - 2])
+                        message_flag = False
+                        break;
+                    else:
+                        message_flag = True
         # 日時を選別
-        if site_info[i] == "d" and site_info[i + 1] == "a" and site_info[i + 2] == "t" and site_info[i + 3] == "e":
+        if site.text[i] == "d" and site.text[i + 1] == "a" and site.text[i + 2] == "t" and site.text[i + 3] == "e" and site.text[i + 4] == "\"":
             if date_flag:
-                # i = i+6
-                for j in range(i, len(site_info)):
-                    if site_info[j] == "2" and site_info[j + 1] == "0" and (site_info[j + 2] == "0" or site_info[j + 2] == "1" or site_info[j + 2] == "2"):
-                        year = int(site_info[j:j + 4])
-                        month = int(site_info[j + 5:j + 7])
-                        day = int(site_info[j + 8:j + 10])
-                        hour = int(site_info[j + 11:j + 13])
-                        minute = int(site_info[j + 14:j + 16])
-                        second = int(site_info[j + 17:j + 19])
+                i = i+6
+                for j in range(i, len(site.text)):
+                    if site.text[j] == "2" and site.text[j + 1] == "0" and (site.text[j + 2] == "0" or site.text[j + 2] == "1" or site.text[j + 2] == "2"):
+                        year = int(site.text[j:j + 4])
+                        month = int(site.text[j + 5:j + 7])
+                        day = int(site.text[j + 8:j + 10])
+                        hour = int(site.text[j + 11:j + 13])
+                        minute = int(site.text[j + 14:j + 16])
+                        second = int(site.text[j + 17:j + 19])
                         commit_date_time = datetime.datetime(year=year, month=month, day=day, hour=hour,
                                                              minute=minute,
                                                              second=second)
                         i = j
                         if compare_flag:
-                            print(str(commit_date_time)+" 次コミットとの時間差："+str(compare_date_time - commit_date_time))
-                            # print(str(compare_date_time - commit_date_time))
+                            git_log.append(" 次コミットとの時間差："+str(compare_date_time - commit_date_time))
+                            git_log.append(str(commit_date_time))
                             compare_date_time = commit_date_time
+                            i = i + 27
                         else:
                             compare_date_time = commit_date_time
                             compare_flag = True
-                            print(str(commit_date_time))
+                            git_log.append(str(commit_date_time))
+                            i = i + 27
                         break;
-                i = i + 27
                 date_flag = False
             else:
+                i = i + 27
                 date_flag = True
-
-        # メッセージを選別
-        if site_info[i] == "m" and site_info[i + 1] == "e" and site_info[i + 2] == "s" \
-                and site_info[i + 3] == "s" and site_info[i + 4] == "a" and site_info[i + 5] == "g" and site_info[i + 6] == "e":
-            message_start = i + 10
-            for message_end in range(message_start, len(site_info)):
-                if site_info[message_end] == "\"":
-                    if message_flag:
-                        print(site_info[message_start:message_end - 2]+"\n")
-                        message_flag = False
-                        break;
-                    else:
-                        message_flag = True
-
+    return git_log
